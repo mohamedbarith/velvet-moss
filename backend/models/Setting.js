cbcdbf21191530e5f92e8433549699c32f35aec8
mohamedbarith <mohamedbarith22td0699@svcet.ac.in>
2026-03-25 15:59:37 +0530

    500 3ndtime

--- backend/models/Setting.js
+++ backend/models/Setting.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Setting = sequelize.define('Setting', {
-    key: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
+    setting_key: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
     value: { type: DataTypes.STRING, allowNull: false },
 }, {
     tableName: 'settings',
