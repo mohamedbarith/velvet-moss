28eaac134465bde9048af7b59f07cf5f2b4eb15a
mohamedbarith <mohamedbarith22td0699@svcet.ac.in>
2026-03-26 22:08:48 +0530

    picture mistake 4"

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cloudinary = require('../config/cloudinary.js');
+const cloudinary = require('../config/cloudinary');
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
